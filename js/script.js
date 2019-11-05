68b7b83a5ac1887e14477460073bacf6b7a9a618
BraydenGray416 <brayden.gray98@gmail.com>
2019-11-05 15:03:00 +1300

    added in projects images and data

--- js/script.js
+++ js/script.js
@@ -4,43 +4,43 @@ const workPieces = [
         title: '#offTopic Geek Culture Store',
         description: 'This is a online store where users can sell and buy geek culture merch.',
         githubLink: 'https://github.com/CyborgSemon/summative-3-front',
-        img: ''
+        image: 'assets/geekculturestore.png'
     },
     {
         id: 2,
-        title: '#offTopic Geek Culture Store',
-        description: 'This is a online store where users can sell and buy geek culture merch.',
-        githubLink: 'https://github.com/CyborgSemon/summative-3-front',
-        img: ''
+        title: 'Pokemon Type Filter',
+        description: 'This is a web app the filters the original 151 pokemon by Type',
+        githubLink: 'https://github.com/BraydenGray416/Pokemon-Filtering',
+        image: 'assets/pokemonFilter.png'
     },
     {
         id: 3,
-        title: '#offTopic Geek Culture Store',
-        description: 'This is a online store where users can sell and buy geek culture merch.',
+        title: 'Grind Digital Ordering System',
+        description: 'This is a digital ordering system for the milkshake bar Grind. Grind is located in Upper Hutt and provides various unique flavors',
         githubLink: 'https://github.com/CyborgSemon/summative-3-front',
-        img: ''
+        image: 'assets/grind.png'
     }
-]
+];
 
-for (var i = 0; i < array.length; i++) {
-    array[i]
+function showProjects(){
+let projectCard = '';
+for (var i = 0; i < workPieces.length; i++) {
+    const projects = workPieces[i];
+
+    projectCard += `<div class="card col-12 ml-2 h-100" style="width: 30rem;">
+    <img src="${projects.image}" class="card-img-top" alt="${projects.title}">
+    <div class="card-body">
+    <h5 class="card-title">${projects.title}</h5>
+    <p class="card-text">${projects.description}</p>
+    <a href="#" class="btn btn-danger workCards" data-id="$projects.id}">Read More</a>
+    <a href="${projects.githubLink}" class="btn btn-dark" >Github</a>
+    </div>
+    </div>`;
+    console.log(projectCard);
 }
-    projectCard = `<div class="col-md-4">
-        <div class="card mb-4 shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>`
 
 
-    $(`projectSection`).html(projectCard);
-})
+document.getElementById('projectSection').innerHTML = projectCard;
+}
+
+showProjects();
